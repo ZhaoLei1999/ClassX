27657ee1255150a14612ae2cac80b467866491b3
Zhaolei <2443380740@qq.com>
2019-07-11 20:14:14 +0800

    niu

--- ClassX/ViewController.swift
+++ ClassX/ViewController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AsyncReponseDelegate {
+    func receviedReponse(_ sender: AsyncRequestWorker, responseString: String, tag: Int) {
+        //print(responseString)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: NSNotification.Name(rawValue: "response.received"), object: nil)
+        
+        
+        
+        let work : AsyncRequestWorker = AsyncRequestWorker()
+        
+        work . reponseDelegate = self
+        
+        work .getResponse(from: "https://google.com", tag: 1)
+        
     }
 
-
+    @objc func keyboardWillShow(notification: NSNotification) {
+        
+        let word = notification.userInfo!["response"]
+        
+        print(word)
+        
+        
+//        let keyboardHeight = (notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue.height
+//        print( "猜猜看：  \(keyboardHeight)" )
+//
+//        btnHiBottomConstraint.constant = keyboardHeight
+    }
 }
 
